3561cb4530054d3d7cf91497128beb2f268a485a
Eduard Boytsov <eduard.boytsov@gmail.com>
2025-04-11 13:26:33 -0400

    feat: grid creation logic and ui

--- MyProject/Assets/Scripts/GameManager.cs
+++ MyProject/Assets/Scripts/GameManager.cs
@@ -7,13 +7,22 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     [Header("GameSettings")]
-    [SerializeField] private TMP_InputField columns;
-    [SerializeField] private TMP_InputField rows;
+    [SerializeField] private TMP_InputField columnsInputField;
+    [SerializeField] private TMP_InputField rowsInputField;
     [SerializeField] private Button beginButton;
+    [SerializeField] private GameObject buttonPrefab;
+    [SerializeField] private GameObject layoutChoicePanel;
 
     [Header("Grid Settings")]
+    [SerializeField] private RectTransform buttonContainer;
     [SerializeField] private Vector2 cellSize = new Vector2(100, 100);
     [SerializeField] private Vector2 spacing = new Vector2(10, 10);
+
+    [Header("Input Constraints")]
+    [SerializeField] private int columnCeiling;
+    [SerializeField] private int columnFloor;
+    [SerializeField] private int rowCeiling;
+    [SerializeField] private int rowFloor;
 
     // Start is called before the first frame update
     void Start()
@@ -25,5 +34,44 @@ public class GameManager : MonoBehaviour
     void Update()
     {
 
+    }
+
+    private void GenerateGrid(int columns, int rows) 
+    {
+        foreach (Transform child in buttonContainer)
+        {
+            Destroy(child.gameObject);
+        }
+
+        GridLayoutGroup gridLayout = buttonContainer.GetComponent<GridLayoutGroup>();
+
+        gridLayout.cellSize = cellSize;
+        gridLayout.spacing = spacing;
+
+        gridLayout.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        gridLayout.constraintCount = columns;
+        
+
+        for (int i = 0; i < rows * columns; i++)
+        {
+            GameObject newButton = Instantiate(buttonPrefab, buttonContainer);
+            newButton.name = $"Button_{i}";
+        }
+    }
+
+    public void OnBegin()
+    {
+        if (int.TryParse(columnsInputField.text, out int columns) && int.TryParse(rowsInputField.text, out int rows))
+        {
+            if (columnCeiling > columns && columnFloor < columns && rowCeiling > rows && rowFloor < rows)
+            {
+                GenerateGrid(columns, rows);
+                layoutChoicePanel.SetActive(false);
+            }
+        }
+        else
+        {
+            Debug.Log("invalid input");
+        }
     }
 }
